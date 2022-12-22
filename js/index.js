ec8dfd34838b21ef189e34397785f53d730cbef4
raquel2002x <palomofuentesraquel2002@gmail.com>
2022-12-22 11:33:30 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const requestURL = "../json/peliculas.json";
+
+
+async function fetchMoviesJson(){
+    const response = await fetch
+    (requestURL);
+    const movies = await response.json();
+    return movies;
+}
+
+fetchMoviesJson().then(movies => {
+    for (let index = 0; index < movies.peliculas.lenght ; index++)
+        {
+            
+        
+            const moviesSection = document.getElementById("movieSection");
+        
+            let id = movies.peliculas[index].id;
+            let img = movies.peliculas[index].caratula;
+            let title = movies.peliculas[index].titulo;
+            let description= movies.peliculas[index].descripcion;
+            let genre = movies.peliculas[index].genero;
+            let director = movies.peliculas[index].director;
+
+        
+
+            moviesSection.innerHTML += `
+                ${director}
+            `
+
+            console.log (movies.peliculas[index].id);
+        
+        };
+})
